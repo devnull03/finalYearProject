28c7edc9cff041217b344beee072cc2c5aae424d
arnav03mehta <arnav2003mehta@gmail.com>
2020-10-22 22:12:26 +0530

    added the send file button and a text box

--- client/mainPage.py
+++ client/mainPage.py
@@ -1,8 +1,7 @@
 import pygame
 import time
-
-from pygame import mouse
-# from modules.pygame_textinput import TextInput
+import os
+from modules.pygame_textinput import TextInput
 from modules.text_wrapper import drawText
 
 
@@ -22,10 +21,14 @@ class MainPage:
         self.Font = pygame.font.SysFont('Times New Roman', 30)
         self.small_font = pygame.font.SysFont('Times New Roman', 20)
         self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
 
         self.mode = kwargs["mode"]
         self.task = kwargs["task"]
         self.examples = kwargs["examples"]
+        self.default_path = kwargs["default_path"]
+        self.file_path = TextInput(font_size=20, max_string_length=60, initial_string=self.default_path)
+        self.path_box_bool = False
 
         self.timerBool = False
         self.t = time.time()
@@ -51,18 +54,14 @@ class MainPage:
                     self.timerBool = False
         else:
             Timer = f"{self.mins + 1}:00"
-
         text = self.Font.render(str(Timer), True, (0, 0, 0))
         self.mainScreen.blit(text, location)
 
     def dev_button(self, events):
-
         pos = (20, 240)
         size = (60, 25)
         mouse = pygame.mouse.get_pos()
-
         pygame.draw.rect(self.mainScreen, (20, 20, 20), pygame.Rect(pos, size))
-
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pos[0] <= mouse[0] <= pos[0] + size[0] and pos[1] <= mouse[1] <= pos[1] + size[1]:
@@ -70,34 +69,39 @@ class MainPage:
                         self.t = time.time()
                     self.timerBool = True
 
-    def finish_button(self, events):
-
-        pos = (200, 640)
-        size = (60, 25)
+    def finish_gui(self, events):
+        pos = (325, 580)
+        size = (95, 29)
         mouse = pygame.mouse.get_pos()
-
         pygame.draw.rect(self.mainScreen, (20, 20, 20), pygame.Rect(pos, size))
+        pygame.draw.rect(self.mainScreen, self.black, pygame.Rect(box_pos:=(20, 581), box_size:=(281, 28)), 1)
+
+        self.mainScreen.blit(self.file_path.get_surface(), (25, 587))
+
+        location = (pos[0]+10, pos[1]+3)
+        text = self.small_font.render("Send file", True, self.white)
+        self.mainScreen.blit(text, location)
 
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if pos[0] <= mouse[0] <= pos[0] + size[0] and pos[1] <= mouse[1] <= pos[1] + size[1]:
-                    return True
+                    self.path_box_bool = False
+                    return self.file_path.get_text()
+                if box_pos[0] <= mouse[0] <= box_pos[0] + box_size[0] and box_pos[1] <= mouse[1] <= box_pos[1] + box_size[1]:
+                    self.path_box_bool = True
+                else:
+                    self.path_box_bool = False
+        if self.path_box_bool:
+            self.file_path.update(events)
 
     def display_info(self):
         black = self.black
-
         mode_location = (200, 19)
         mode_text = self.Font.render(f"Mode - {self.mode}", True, black)
         self.mainScreen.blit(mode_text, mode_location)
-
-        drawText(
-            self.mainScreen, self.task, self.black, ((25, 115), (427, 227)), self.small_font
-            )
-
-        
+        drawText(self.mainScreen, self.task, self.black, ((25, 115), (427, 227)), self.small_font)
 
     def test_start(self):
-
         while 1:
             events = pygame.event.get()
             for event in events:
@@ -105,35 +109,32 @@ class MainPage:
                     pygame.quit()
                     return False
 
-            # pos = pygame.mouse.get_pos()
-            # if pos != self.mousePos:
-            #     print(pos)
-            #     self.mousePos = pos
-            # mainScreen.fill((170, 170, 170))
+            if 0:
+                pos = pygame.mouse.get_pos()
+                if pos != self.mousePos:
+                    print(pos)
+                    self.mousePos = pos
+                self.mainScreen.fill((170, 170, 170))
 
             self.mainScreen.blit(self.back, (0, 0))
             self.display_info()
             self.dev_button(events)
             self.timer()
-
-            '''
-            if self.timerBool:
-                self.timer()
-            else:
-                text = self.Font.render(f"{self.mins + 1}:00", True, (0, 0, 0))
-                self.mainScreen.blit(text, (33, 20))
-            '''
+            if a:=self.finish_gui(events):
+                print(a)
 
             pygame.display.update()
             self.clock.tick(30)
 
 
 if __name__ == "__main__":
-
+    os.chdir(f"{os.getcwd()}\\client")
     d = {
         "mode": "Shortest",
         "time": 1,
-        "task": "Test task ........... .............. .........."
+        "task": "Test task ........... .............. ..........",
+        "examples": None,
+        "default_path": "C:\\Users\\Dell\\OneDrive\\Desktop"
     }
 
     MainPage(**d).test_start()
