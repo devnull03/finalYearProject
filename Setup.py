66b28b3320fb7f0c81b082657290e760ebf3883e
arnav03mehta <arnav2003mehta@gmail.com>
2020-11-11 18:36:09 +0530

    Added nonetype protection

--- Setup.py
+++ Setup.py
@@ -1,4 +1,5 @@
-import os, winshell
+from client import settings
+import os, winshell, sys
 from win32com.client import Dispatch
 
 desktop = winshell.desktop()
@@ -12,10 +13,23 @@ def mkshortcut(target, wDir, icon, link_name):
     shortcut.IconLocation = icon
     shortcut.save()
 
-info = {'server': "Clash of Code - Server", 'client': "Clash of Code"}
+def set_server(addr, port):
+    if port == "":
+        port = settings.PORT
+    with open("client/settings.py", "w") as settings_file:
+        settings_file.write(f"\nSERVER = \"{addr}\" \nPORT = {port} \n ")
 
+info = {'server': "Clash of Code - Server", 'client': "Clash of Code"}
 for i in info:
     target = f"{os.getcwd()}\\{i}\\{i}.py"
     wDir = f"{os.getcwd()}\\{i}"
     icon = f"{os.getcwd()}\\{i}\\assets\\logo.ico"
     mkshortcut(target, wDir, icon, info[i])
+
+server = input("[Press enter to skip] Server address: ")
+if server == "":
+    sys.exit()
+port = input("[Press enter to use default] Port to be used: ")
+
+# set_server("192.168.29.105", 6969)
+set_server(server, port)
