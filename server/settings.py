41f51fd65d6f25d3139d3bd8880ccb3d7c2ba42d
arnav03mehta <arnav2003mehta@gmail.com>
2020-11-20 00:42:00 +0530

    Added more explanations

--- server/settings.py
+++ server/settings.py
@@ -1,29 +1,38 @@
 import os
+# ------Code used while development------
 # cwd = os.getcwd()
 # if not cwd.endswith('\\server'):
 #     os.chdir(f"{cwd}\\server")
+# ---------------------------------------
 
+# 
 # # EXAMPLE_FILE : 
 #     Path to the file that will be 
 #     sent to the players as a reference program
+#     ( Should be named example.py )
 # 
 # SOLUTION_FILE :
 #     Path to the file that contains a working solution,
 #     which will be used to test player's functions
-# 
+#     ( Should be named check.py )
+#
 # TEST_CASES :
 #      Path to the file that contains all the testcases
+#     ( Should be named test_cases.py )
 # 
 
-presets_folder = os.getcwd() + "\\presets"
-presets = {
+presets_folder = os.getcwd() + "\\presets" # do not change
+presets = {         # Format for adding new presets:-
+                    # 'ChallengeName': f"{presets_folder}\\ChallengeFileName",
     'CardTiming': f"{presets_folder}\\CardTiming",
     'Treasure': f"{presets_folder}\\Treasure"
 }
-PRESET = 'Treasure'
+PRESET = 'Treasure' # Choose from the presets dictionary
 
 # Server files ------------------------------------
-folder = os.getcwd().rstrip('\\server') + "\\test_files"
+folder = os.getcwd().rstrip('\\server') + "\\test_files" # Change folder location to the one 
+                                                         # containing your files
+
 if PRESET:
     folder = presets[PRESET]
 
@@ -34,7 +43,8 @@ TEST_CASES = f"{folder}\\test_cases.py"
 #--------------------------------------------------
 # 
 # MODE :
-#     There is currently only one option, i.e. Shortest
+#     There are currently only two options, i.e. Shortest, Fastest.
+#     Shortest sorts by file size and Fastest sorts by time taken
 # TIME :
 #     The amount of time availabe for players to write their code
 # TASK :
@@ -46,23 +56,30 @@ TEST_CASES = f"{folder}\\test_cases.py"
 #     dictionary, i.e. { <input>: <output>, <input>: <output> }
 #
 #     If you choose a preset, TASK and EXAMPLES will automatically be 
-#     imported according to the preset
+#     imported according to the preset, you can leave those blank, i.e. None, or as is,
+#     just without any syntax error
 #
 # Challenge Info ----------------------------------
 
-MODE = "Shortest"
+MODE = "Shortest" # the spelling should be correct, case doesnt matter
 TIME = 10
 
-TASK = "test task"*10
-EXAMPLES = {
+# Use similar format for writing the task
+TASK = """test task
+"""*10
+
+# If input is a list or tuple or dictionry, make it a string
+EXAMPLES = {    
     1: 1,
     2: 4,
     3: 9
 }
 
+# -----------Do not change------------
 if PRESET:
-    from presets import info
+    from presets import info  # ClashOfCode\\server\\presets\\info.py
     TASK = info.INFO[PRESET]['TASK']
     EXAMPLES = info.INFO[PRESET]['EXAMPLES']
+# ------------------------------------
 
 #--------------------------------------------------
